9183c33d79c8df0ff425bbdeface0735f4fbea18
sn1f3rt <sn1f3rt@outlook.com>
2022-03-06 20:06:17 +0530

    Attempt to fix concurrency

--- discord/ext/ipc/client.py
+++ discord/ext/ipc/client.py
@@ -1,5 +1,7 @@
 import asyncio
 import logging
+import os
+import weakref
 
 import aiohttp
 from .errors import *
@@ -18,7 +20,8 @@ class Client:
     port: int
         The port of the IPC server. If not supplied the port will be found automatically, defaults to None
     secret_key: Union[str, bytes]
-        The secret key for your IPC server. Must match the server secret_key or requests will not go ahead, defaults to None
+        The secret key for your IPC server. Must match the server secret_key or requests will not go ahead,
+        defaults to None
     """
 
     def __init__(
@@ -38,6 +41,11 @@ class Client:
         self.multicast = None
 
         self.multicast_port = multicast_port
+
+        self._requests = weakref.WeakValueDictionary()
+        self._queue = asyncio.Queue()
+        self._worker = None
+        self._sender = None
 
     @property
     def url(self):
@@ -85,58 +93,99 @@ class Client:
         )
         log.info("Client connected to %s", self.url)
 
+        if self._worker is None:
+            self._worker = self.loop.create_task(self._receive_requests())
+        if self._sender is None:
+            self._sender = self.loop.create_task(self._send_requests())
+
         return self.websocket
 
-    async def request(self, endpoint, **kwargs):
+    async def request(self, endpoint: str, timeout: float = 60.0, **kwargs):
         """Make a request to the IPC server process.
 
         Parameters
         ----------
         endpoint: str
             The endpoint to request on the server
+        timeout: float
+            The number of seconds to wait for a response from the server
+            before timing out and raising :exc:`asyncio.TimeoutError`.
+            By default this is 60 seconds.
         **kwargs
             The data to send to the endpoint
+
+        Raises
+        -------
+        asyncio.TimeoutError
+            If a timeout was reached.
         """
         log.info("Requesting IPC Server for %r with %r", endpoint, kwargs)
         if not self.session:
             await self.init_sock()
 
+        nonce = os.urandom(16).hex()
+
         payload = {
             "endpoint": endpoint,
             "data": kwargs,
             "headers": {"Authorization": self.secret_key},
+            "nonce": nonce,
         }
 
-        await self.websocket.send_json(payload)
+        self._queue.put_nowait(payload)
+        self._requests[nonce] = future = self.loop.create_future()
+        return await asyncio.wait_for(future, timeout)
 
-        log.debug("Client > %r", payload)
+    async def _send_requests(self):
+        while True:
+            payload = await self._queue.get()
+            await self.websocket.send_json(payload)
+            log.debug("Client > %r", payload)
 
-        recv = await self.websocket.receive()
+    async def _receive_requests(self):
+        while True:
+            recv = await self.websocket.receive()
+            log.debug("Client < %r", recv)
 
-        log.debug("Client < %r", recv)
+            if recv.type == aiohttp.WSMsgType.PING:
+                log.info("Received request to PING")
+                await self.websocket.ping()
 
-        if recv.type == aiohttp.WSMsgType.PING:
-            log.info("Received request to PING")
-            await self.websocket.ping()
+            elif recv.type == aiohttp.WSMsgType.PONG:
+                log.info("Received PONG")
 
-            return await self.request(endpoint, **kwargs)
+            elif recv.type == aiohttp.WSMsgType.CLOSED:
+                log.error(
+                    "WebSocket connection unexpectedly closed. IPC Server is unreachable. "
+                    "Attempting reconnection in 5 seconds."
+                )
 
-        if recv.type == aiohttp.WSMsgType.PONG:
-            log.info("Received PONG")
-            return await self.request(endpoint, **kwargs)
+                await self.session.close()
 
-        if recv.type == aiohttp.WSMsgType.CLOSED:
-            log.error(
-                "Websocket connection closed unexpectedly. IPC server is unreachable. "
-                "Attempting reconnection in 5 seconds."
-            )
+                await asyncio.sleep(5)
 
-            await self.session.close()
+                await self.init_sock()
 
-            await asyncio.sleep(5)
+            else:
+                response = recv.json()
+                nonce = response["nonce"]
 
-            await self.init_sock()
+                try:
+                    future = self._requests[nonce]
+                except KeyError:
+                    log.error("Received unknown request: %r", response)
+                else:
+                    ret = response.get("response", response)
+                    future.set_result(ret)
 
-            return await self.request(endpoint, **kwargs)
+    async def close(self):
+        """Close the connection to the websocket."""
 
-        return recv.json()
+        await self.session.close()
+
+        if self._worker is not None:
+            self._worker.cancel()
+            self._worker = None
+        if self._sender is not None:
+            self._sender.cancel()
+            self._sender = None
--- discord/ext/ipc/server.py
+++ discord/ext/ipc/server.py
@@ -178,42 +178,64 @@ class Server:
             log.debug("IPC Server < %r", request)
 
             endpoint = request.get("endpoint")
-
             headers = request.get("headers")
+            nonce = request.get("nonce")
 
             if not headers or headers.get("Authorization") != self.secret_key:
                 log.info(
                     "Received unauthorized request (Invalid or no token provided)."
                 )
-                response = {"error": "Invalid or no token provided.", "code": 403}
+                response = {
+                    "nonce": nonce,
+                    "response": {"error": "Invalid or no token provided.", "code": 403},
+                }
+
             else:
                 if not endpoint or endpoint not in self.endpoints:
                     log.info("Received invalid request (Invalid or no endpoint given).")
-                    response = {"error": "Invalid or no endpoint given.", "code": 400}
+                    response = {
+                        "nonce": nonce,
+                        "response": {
+                            "error": "Invalid or no endpoint given.",
+                            "code": 400,
+                        },
+                    }
+
                 else:
                     server_response = IpcServerResponse(request)
 
                     try:
                         ret = await self.endpoints[endpoint](server_response)
-                        response = ret
+
                     except Exception as error:
                         log.error(
                             "Received error while executing %r with %r",
                             endpoint,
                             request,
                         )
+
                         self.bot.dispatch("ipc_error", endpoint, error)
 
                         response = {
-                            "error": "IPC route raised error of type {}".format(
-                                type(error).__name__
-                            ),
-                            "code": 500,
+                            "nonce": nonce,
+                            "response": {
+                                "error": "IPC route raised error of type {}".format(
+                                    type(error).__name__
+                                ),
+                                "code": 500,
+                            },
+                        }
+
+                    else:
+                        response = {
+                            "nonce": nonce,
+                            "response": ret
                         }
 
             try:
                 await websocket.send_json(response)
                 log.debug("IPC Server > %r", response)
+
             except TypeError as error:
                 if str(error).startswith("Object of type") and str(error).endswith(
                     "is not JSON serializable"
@@ -225,7 +247,10 @@ class Server:
                     )
                     log.error(error_response)
 
-                    response = {"error": error_response, "code": 500}
+                    response = {
+                        "nonce": nonce,
+                        "response": {"error": error_response, "code": 500},
+                    }
 
                     await websocket.send_json(response)
                     log.debug("IPC Server > %r", response)
